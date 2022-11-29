9a5ae3765cd8718a45b73d3c49baefea06a0f82b
Aliakparovna <alliakparovnaa2004@gmail.com>
2022-11-29 12:25:36 +0300

    home work 4

--- src/App.js
+++ src/App.js
@@ -1,24 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+//1-вариант
+// import React from "react";
+// import "./App.css";
+
+// function App() {
+
+
+//   let a = 5;
+
+//   let textInput = React.createRef()
+//   let textOut = React.createRef()
+
+//   const onAdd =  (props) => {
+    
+//     console.log('Home work' + props);
+//   }
+
+//   const showInput = (event) =>{
+//     console.log('input');
+//     // console.log(event.target.value);
+//     console.log(textInput.current.value)
+//     textOut.current.innerHTML = textInput.current.value
+
+//   }
+//   return (
+//     <>
+//       <h1> Home work 5</h1>
+//       <section>
+//       <h2>Клик</h2>
+//       <button onClick={() => onAdd(4)}> Push </button>
+//       </section>
+
+//       <section>
+//       <h2>Двойной клик </h2>
+//       <div className="btn" onDoubleClick={() => onAdd(5)} > Щелкните дважды </div>
+//       </section>
+
+//       <section>
+//       <h2>Input </h2>
+//       <input type = 'text' onInput={showInput} ref = {textInput}  />
+//       <p ref={textOut}> </p>
+//       </section>
+
+     
+//     </>
+//   );
+// }
+
+// export default App;
+
+//2-вариант
+import React, { useState } from "react";
+import "./App.css";
+
 
 function App() {
+
+
+  let a = 5;
+
+  let textInput = React.createRef()
+  let textOut = React.createRef()
+  const [outPut, setOutput] = useState ('0')
+
+  const onAdd =  (props) => {
+    
+    console.log('Home work' + props);
+  }
+
+  const showInput = (event) =>{
+    console.log('input');
+    // console.log(event.target.value);
+    console.log(textInput.current.value)
+    textOut.current.innerHTML = textInput.current.value
+    setOutput(textInput.current.value)
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1> Home work 5</h1>
+      <section>
+      <h2>Клик</h2>
+      <button onClick={() => onAdd(4)}> Push </button>
+      </section>
+
+      <section>
+      <h2>Двойной клик </h2>
+      <div className="btn" onDoubleClick={() => onAdd(5)} > Щелкните дважды </div>
+      </section>
+
+      <section>
+      <h2>Input </h2>
+      <input type = 'text' onInput={showInput} ref = {textInput}  />
+      <p ref={textOut}> </p>
+      <h3> {outPut}</h3>
+      </section>
+
+     
+    </>
   );
 }
 
